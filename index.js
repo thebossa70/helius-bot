8a209e4955fb2e9c4dc2712927426da79d1401fb
luism <thebossa65@gmail.com>
2026-04-27 22:11:51 -0400

    fix bot completo

--- index.js
+++ index.js
@@ -17,12 +17,34 @@ const CHAT_ID = process.env.CHAT_ID || "1998268076";
 
 const WATCH_WALLETS = [
  "bwamJzztZsepfkteWRChggmXuiiCQvpLqPietdNfSXa",
- "BigrQYqqumRdxseNJwqwaAR86zeEVjKpVuXMaEucqqTu"
+ "FxwArENkKBx4QyfoEU1vkBnDzMfZV9Z1b8GBzpT9zb5k",
+ "7eStM4BgV5e3AvE1DXHwvGh5M9PoRvExNAYS98LjeSym",
+ "86jut9tAf9gPfkaUhwEjN8avhH7UxuoGUyk7oeZwtoV7",
+ "4qBro3VYM7ZcDeLTcGueazKYQYED6ZmM3HEy7NxTuNFU",
+ "F5XvCe4233m6mHRbkkq2ZsFvqrPAnRrDBExeQy2fwagQ",
+ "BCagckXeMChUKrHEd6fKFA1uiWDtcmCXMsqaheLiUPJd",
+ "662dsE2yrBXzjYFyQvqjxiXfjUhqxo52aGcJFaVy7puc",
+ "HsuDfzmCaPBeCBdj1LaCR1yNn7wrCnXNHaYYCs3m7X28",
+ "rNAUS5wm8CqFodvUaU1qqiBJ594uVn3heCLtTnHrjpw",
+ "CuZUpmhFsz9t1ERWfNubzmwrjRCjn9WSkz6qg6BSSyrf",
+ "7yU2QHwhWFjJFcCYDcUFCHB898qSfFqtificLidS8cp2",
+ "5R4Z9SKfe9hUiU4MAVBgE2Cjqdwhy8H9o2GBVCC1YxF2",
+ "A6nm8LqpeC76a5H31GZLttWY2KqHVH9H8ASYvEPudSYp",
+ "AeBgYrBba5Cq38HR7G6quKairkyCpBk5Nt2kKyaEFZUr",
+ "9UWsdEWgixs7qCZKgEXq9ifs7KkDu8xW9zGopiVaLVj7",
+ "DrwtvGAhrWutCWYQqHT4zceietfBJTVUBjZVbR2sNiJq",
+ "4qBro3VYM7ZcDeLTcGueazKYQYED6ZmM3HEy7NxTuNFU",
+ "9FtGm6hJULCpA8An4sFg5ysHUExDZBtMeDCxsYnTnWh5"
 ].map(w => w.toLowerCase());
 
 
 const TARGET_WALLETS = [
  "BigrT9DAnXnzVNFQPg3VF4WvyGyifFtFMVALPVsLnvTu",
+ "GmMQP6KgWhZHaxVScGYULJp26JsdgjPBMwmS3SDyZhnS",
+ "9cWia2oyxRmK1suBQ9foQ94n3mz5GcE17RRzKHQm3PwD",
+ "4qBro3VYM7ZcDeLTcGueazKYQYED6ZmM3HEy7NxTuNFU",
+ "F5XvCe4233m6mHRbkkq2ZsFvqrPAnRrDBExeQy2fwagQ",
+ "51yZyDSnec4xnUv7XLRVYcDyV4x3wUtzrDcRaYbmQU5j"
 ].map(w => w.toLowerCase());
 
 
